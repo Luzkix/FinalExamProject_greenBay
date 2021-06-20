d498b89372da96c99fb8bcc0596d11b473ae8a88
Zdenek L <luzkix@seznam.cz>
2021-06-20 21:37:21 +0200

    refactor: some formal adjustements and checkstyle corrections

--- src/main/java/com/greenfoxacademy/greenbayapp/bid/services/BidServiceImpl.java
+++ src/main/java/com/greenfoxacademy/greenbayapp/bid/services/BidServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class BidServiceImpl implements BidService {
   private BidRepository bidRepository;
   private ProductService productService;
